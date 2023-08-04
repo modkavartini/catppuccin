831df1410b4fd920930a92c13be22f5c8edd89db
abhiram <81793953+modkavartini@users.noreply.github.com>
2023-08-04 11:01:12 +0530

    testing

--- @resources/inc/update.ps1
+++ @resources/inc/update.ps1
@@ -1,14 +1,18 @@
 function goBrrr {
     $githubName="modkavartini"
     $skinName="catppuccin"
-    $latestVer=$RmAPI.MeasureStr("latestVer")
+    $latestVerRaw=Invoke-WebRequest -Uri "https://api.github.com/repos/$githubName/$skinName/releases/latest" -UseBasicParsing
+    $latestVer=($latestVerRaw | ConvertFrom-Json).tag_name -replace "v",""
     $url="https://github.com/$githubName/$skinName/releases/download/v$latestVer/$skinName"+"_$latestVer.rmskin"
     $outPath="C:/Windows/Temp/$skinName"+"_$latestVer.rmskin"
 
+    Write-Host "> Downloading $skinName..." -ForegroundColor "Yellow"
     $wc=New-Object System.Net.WebClient
     $wc.DownloadFile($url, $outPath)
     $shell=New-Object -ComObject Shell.Application
     $shell.minimizeall()
+    Stop-Process -Name "Rainmeter"
+    Write-Host "> Installing v$latestVer..." -ForegroundColor "Yellow"
     Start-Process $outPath
 
     if($null -notMatch (Get-Process "SkinInstaller" -ea 0)) {
@@ -16,4 +20,6 @@ function goBrrr {
         $wshell=New-Object -ComObject wscript.shell
         $wshell.SendKeys('~')
     }
+    Write-Host "> The $skinName suite was installed successfully." -ForegroundColor "Green"
 }
+goBrrr
