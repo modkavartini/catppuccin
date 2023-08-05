c4e0181e3d46c95cefbdf3b6b217ae30783a15d7
abhiram <81793953+modkavartini@users.noreply.github.com>
2023-08-05 11:44:49 +0530

    skull

--- @resources/inc/calendar.ps1
+++ @resources/inc/calendar.ps1
@@ -1,11 +1,11 @@
 function setInline {
     $RmAPI.Bang("!setoptiongroup date inlinesetting none")
     $inline=$RmAPI.VariableStr("iSetting")
+    $sDate=$RmAPI.Measure("setDate")
+    $mEnd=$RmAPI.Measure("mEnd")
+    $mMonth=$RmAPI.Variable("mMonth")
     for($d=0; $d -lt 43; $d++) {
         $date=$RmAPI.VariableStr("$d")
-        $sDate=$RmAPI.Measure("setDate")
-        $mEnd=$RmAPI.Measure("mEnd")
-        $mMonth=$RmAPI.Variable("mMonth")
         $eText=$RmAPI.VariableStr("$date" + ".$mMonth")
         if($null -notMatch [regex]::escape($eText)) {
             $RmAPI.Bang("!setoption $d inlinesetting $inline")
